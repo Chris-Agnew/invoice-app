09a9bf474623b090d15a6fe8768e768516d3135c
chris-agnew <hello@chrisagnew.codes>
2022-12-08 05:29:47 -0500

    chore(project setup) set up design system and configure jest for unit testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "medium-slate-blue": "#7C5DFA",
+        "tropical-indigo": "#9277FF",
+        "dark-byzantine-blue": "#1E2139",
+        "lighter-dark-byzantine-blue": "#252945",
+        "lavender-mist": "#DFE3FA",
+        "cool-gray": "#888EB0",
+        "wild-blue-yonder": "#7E88C3",
+        eigengrau: "#0C0E16",
+        "grapefruit-red": "#EC5757",
+        "salmon-pink": "#FF9797",
+        "light-background": "#F8F8FB",
+        "light-eigengrau": "#141625",
+      },
+      fontFamily: {
+        spartan: ["Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
